d057158f4dd4d825f988b5ce371b7959d66033fb
Filipe <filipegrodriguesreal@gmail.com>
2023-10-30 21:16:38 -0300

    FEAT: Adicionei Pré-Cadastro (NOT FINISHED)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PreCadastroComponent } from './pre-cadastro/pre-cadastro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CadastroComponent
+    CadastroComponent,
+    PreCadastroComponent
   ],
   imports: [
     BrowserModule,
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,14 @@ module.exports = {
       colors:{
         'blue-base':'#050934',
         'gray-text': '#565555',
-        'gray-div': '#D9D9D9'
+        'gray-div': '#D9D9D9',
+        'gray-text': '#767474'
       },
       borderColor: {
         'blue-base': '#050934'
+      },
+      height: {
+        '100': '30rem'
       }
     },
   },
